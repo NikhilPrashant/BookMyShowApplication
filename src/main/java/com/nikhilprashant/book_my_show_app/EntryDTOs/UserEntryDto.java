efd19b9f9ae2938330114a048419319de6dcc72d
nikhil-racloop <nikhilprashant1@gmail.com>
2023-06-28 14:13:35 +0530

    junit test

--- src/main/java/com/nikhilprashant/book_my_show_app/EntryDTOs/UserEntryDto.java
+++ src/main/java/com/nikhilprashant/book_my_show_app/EntryDTOs/UserEntryDto.java
@@ -1,8 +1,10 @@
 package com.nikhilprashant.book_my_show_app.EntryDTOs;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
 
 @Data
+@AllArgsConstructor
 public class UserEntryDto {
 
     private String name;
--- src/test/java/com/nikhilprashant/book_my_show_app/BookMyShowAppApplicationTests.java
+++ src/test/java/com/nikhilprashant/book_my_show_app/BookMyShowAppApplicationTests.java
@@ -1,13 +1,35 @@
 package com.nikhilprashant.book_my_show_app;
 
+import com.nikhilprashant.book_my_show_app.Controller.UserController;
+import com.nikhilprashant.book_my_show_app.Entities.UserEntity;
+import com.nikhilprashant.book_my_show_app.EntryDTOs.UserEntryDto;
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.ArrayList;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest
 class BookMyShowAppApplicationTests {
 
+	@Autowired
+	UserController userController;
+
 	@Test
 	void contextLoads() {
+	}
+
+	@Test
+	public void testGetExpectedObject() {
+		Response response = RestAssured.get("localhost:8080/users/add");
+		String expected = "User added successfully";
+		String actual = userController.createUser(new UserEntryDto("test1", 20, "test1@testemail.com", "2134567890", "test1 address")).getBody().toString();
+		assertEquals(expected, actual, "Unexpected status code");
+
 	}
 
 }
